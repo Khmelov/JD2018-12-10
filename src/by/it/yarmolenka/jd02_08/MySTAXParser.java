e7e00772f0f44febb9faeb8a1a48be8672b5b398
yarmolenka <andrey23081989@mail.ru>
2019-02-12 20:12:40 +0300

    HomeWork 02-08 complete

--- src/by/it/yarmolenka/jd02_08/MySTAXParser.java
+++ src/by/it/yarmolenka/jd02_08/MySTAXParser.java
@@ -4,7 +4,6 @@ import by.it.yarmolenka.MathCalc.GetPath;
 import by.it.yarmolenka.jd02_07.UsersValidator;
 
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.*;
@@ -15,50 +14,13 @@ class MySTAXParser {
         XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
         try (Reader reader = new BufferedReader(new FileReader(path))) {
             XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(reader);
-            StringBuilder xmlText = process(xmlStreamReader);
+            STAXHelper helper = new STAXHelper();
+            StringBuilder xmlText = helper.buildSBFromXML(xmlStreamReader);
             System.out.println(xmlText);
         } catch (IOException | XMLStreamException e) {
             e.printStackTrace();
         }
     }
 
-    private static StringBuilder process(XMLStreamReader xml) throws XMLStreamException {
-        StringBuilder textXML = new StringBuilder();
-        StringBuilder insideText = new StringBuilder();
-        String tab = "";
-        while (xml.hasNext()) {
-            int type = xml.next();
-            switch (type) {
-                case XMLStreamConstants.START_ELEMENT:{
-                    textXML.append(tab).append('<').append(xml.getLocalName());
-                    int attrCount = xml.getAttributeCount();
-                    for (int i = 0; i < attrCount; i++) {
-                        String attributeName = xml.getAttributeLocalName(i);
-                        String attributeValue = xml.getAttributeValue(i);
-                        String attributeText = String.format(" %s=\"%s\"", attributeName, attributeValue);
-                        textXML.append(attributeText);
-                        if (attributeText.length()>30 && attrCount-i>1) textXML.append('\n').append(tab);
-                    }
-                    textXML.append(">\n");
-                    tab += "\t";
-                    break;
-                }
-                case XMLStreamConstants.END_ELEMENT:{
-                    String text = insideText.toString().trim();
-                    if (!text.isEmpty()){
-                        textXML.append(tab).append(text).append('\n');
-                        insideText.setLength(0);
-                    }
-                    tab = tab.substring(1);
-                    textXML.append(tab).append("</").append(xml.getLocalName()).append(">\n");
-                    break;
-                }
-                case XMLStreamConstants.CHARACTERS:{
-                    insideText.append(xml.getText());
-                    break;
-                }
-            }
-        }
-        return textXML;
-    }
+
 }
